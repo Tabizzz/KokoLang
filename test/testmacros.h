99e05b3a825519f13ac527bb2abce52bc1b98ad0
Tabizzz <tabatha.reds@gmail.com>
2023-02-08 15:09:59 -0500

    Added segvcatch

--- test/testmacros.h
+++ test/testmacros.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "KokoLang.h"
+#include "segvcatch.h"
 
 #define STR_EQUALS(x, y) (KLCAST(kl_string, src)->size == KLCAST(kl_string, dest)->size && \
 strncmp(KASSTR(x), KASSTR(y), KLCAST(kl_string, x)->size) == 0)
@@ -11,11 +13,23 @@ auto count##x = y;                      \
 while(count##x[x]) {x++;}             \
 delete[] count##x;
 
+static bool segv_init = false;
 
 static inline bool is_valid(KlObject* obj)
 {
-	if(!obj->type) return false;
-	if(!obj->type->klbase.type) return false;
-	if(obj->type->klbase.type == &klBType_Type) return false;
+	if(!segv_init) {
+		segvcatch::init_segv();
+		segv_init = true;
+	}
+	try
+	{
+		if(!obj->type) return false;
+		if(!obj->type->klbase.type) return false;
+		if(obj->type->klbase.type == &klBType_Type) return false;
+	}
+	catch (...)
+	{
+		return false;
+	}
 	return true;
 }
