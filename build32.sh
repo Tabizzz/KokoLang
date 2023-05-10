e31dadc371939189ff3eb1462c03b65f3463d7e9
Tabizzz <tabatha.reds@gmail.com>
2023-05-10 03:14:26 -0500

    add cpack to scripts

--- build32.sh
+++ build32.sh
@@ -4,3 +4,5 @@ _vcpkg=/hdd/Dev/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_TOOLCHAIN_FILE=$_vcpkg -G Ninja -S . -B build/release/x86 -DTARGET_32B=True -DVCPKG_TARGET_TRIPLET=x86-linux
 cmake --build build/release/x86 --target klhost -j 10
+cd build/release/x86
+cpack
--- build64.sh
+++ build64.sh
@@ -4,3 +4,5 @@ _vcpkg=/hdd/Dev/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_TOOLCHAIN_FILE=$_vcpkg -G Ninja -S . -B build/release/x64 -DVCPKG_TARGET_TRIPLET=x64-linux
 cmake --build build/release/x64 --target klhost -j 10
+cd build/release/x64
+cpack
